9431989f054cb78e86ad16d3d27bf2a9dfde75b6
Daniel <daniel.perez.atanacio@gmail.com>
2016-08-14 10:34:40 -0500

    dev: Create step two of the register moneybox - final

--- resources/views/moneybox/create.blade.php
+++ resources/views/moneybox/create.blade.php
@@ -1,29 +1,32 @@
 @extends("layouts.content")
 
 @section("page-content")
-    <section class="block register moneybox" id="register">
+    <section class="block moneybox-view" id="register">
         <div class="holder">
             <div class="container-fluid">
 
                 <div class="header-block">
-                    <div class="title"><h2>Información</h2></div>
-                    <div class="stages-control"></div>
+                    <div class="stage-item">
+                        <div class="stage-item-title">Información</div>
+                        <div class="icon-step icon-step-1"></div>
+                        <div class="label active">Información</div>
+                        <div class="label">Participación</div>
+                    </div>
                 </div>
 
                 <div class="content-block">
-                    <form action="#" class="form register moneybox">
+                    <form action="#" class="form">
                         <div class="form-group">
                             <p class="text-info">Llena por favor los siguientes datos:</p>
                         </div>
-                        <div class="form-group category">
+                        <div class="form-group categories">
                             <label>Selecciona la categoría para la alcancía:</label>
                             <div class="category-items">
-
                                 @foreach($categories as $category)
-                                    <div class="radio-item">
+                                    <label for="chk-{{$category->id}}" class="category-item">
                                         <img src="{{$category->path}}" alt="{{$category->name}}">
-                                        <label for="birth"><input id="birth" type="radio" name="category"> {{$category->name}}</label>
-                                    </div>
+                                        <div class="label"><input id="chk-{{$category->id}}" type="radio" name="category"> {{$category->name}}</div>
+                                    </label>
                                 @endforeach
                             </div>
                         </div>
@@ -39,7 +42,10 @@
                                 </div>
 
                                 <div class="form-group">
-                                    <input id="file" name="file" type="file">
+                                    <div class="row">
+                                        <div class="col-xs-2"><span class="icon-file"></span></div>
+                                        <div class="col-xs-10"><input id="file" name="file" type="file"></div>
+                                    </div>
                                 </div>
 
                                 <div class="form-group">
@@ -52,7 +58,10 @@
                                 </div>
 
                                 <div class="form-group">
-                                    <input id="ended" name="ended" type="date" class="form-control" placeholder="Fecha límite para reunir los fondos">
+                                    <div class="row">
+                                        <div class="col-xs-10"><input id="ended" name="ended" type="date" class="form-control" placeholder="Fecha límite para reunir los fondos"></div>
+                                        <div class="col-xs-2"><span class="icon-calendar"></span></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
--- resources/views/moneybox/step-2.blade.php
+++ resources/views/moneybox/step-2.blade.php
@@ -1,64 +1,73 @@
 @extends("layouts.content")
 
 @section("page-content")
-    <section class="block register step-2" id="register">
+    <section class="block moneybox-view step-2" id="register">
         <div class="holder">
             <div class="container-fluid">
 
                 <div class="header-block">
-                    <div class="title"><h2>Información</h2></div>
-                    <div class="stages-control"></div>
+                    <div class="stage-item">
+                        <div class="stage-item-title">Participación</div>
+                        <div class="icon-step icon-step-2"></div>
+                        <div class="label">Información</div>
+                        <div class="label active">Participación</div>
+                    </div>
                 </div>
 
                 <div class="content-block">
                     <form action="#" class="form register moneybox">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="form-group">
-                                    <label>¿Cómo quiéres que sea el monto de participación?</label>
-                                    <div class="radio">
-                                        <label for="libre">
-                                            <input id="libre" name="way" type="radio"> Libre
-                                        </label>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label>¿Cómo quiéres que sea el monto de participación?</label>
+                                        <div class="radio">
+                                            <label for="libre">
+                                                <input id="libre" name="way" type="radio"> Libre
+                                            </label>
+                                        </div>
+                                        <div class="radio">
+                                            <label for="sugerido" class="flex-element">
+                                                <input id="sugerido" name="way" type="radio">
+                                                <span>Sugerido</span>
+                                                <input class="form-control" id="txtSugerido" name="txtSugerido" type="number" value="0">
+                                                <strong> .00 MXN</strong>
+                                            </label>
+                                        </div>
+                                        <div class="radio">
+                                            <label for="fijo" class="flex-element">
+                                                <input id="filo" name="fijo" type="radio">
+                                                <span>Fijo</span>
+                                                <input class="form-control" id="txtWay" name="txtWay" type="number" value="200">
+                                                <strong>.00 MXN</strong>
+                                            </label>
+                                        </div>
                                     </div>
-                                    <div class="radio">
-                                        <label for="sugerido">
-                                            <input id="sugerido" name="way" type="radio"> Sugerido
-                                            <input id="txtSugerido" name="txtSugerido" type="text" value="$ 000"> <strong> .00 MXN</strong>
-                                        </label>
-                                    </div>
-                                    <div class="radio">
-                                        <label for="fijo">
-                                            <input id="filo" name="fijo" type="radio"> Fijo
-                                            <input id="txtFijo" name="way" type="text" value="$ 000"> <strong> .00 MXN</strong>
-                                        </label>
-                                    </div>
-                                </div>
 
-                                <div class="form-group">
-                                    <label>Opciones de privacidad</label>
-                                    <div class="radio">
-                                        <label for="hidden">
-                                            <input id="hidden" name="privacy" type="radio"> Ocultar la identidad de los participantes
-                                        </label>
-                                    </div>
-                                    <div class="radio">
-                                        <label for="hidden">
-                                            <input id="hidden" name="privacy" type="radio"> Ocultar el importe con el que colaboran los participantes
-                                        </label>
-                                    </div>
-                                    <div class="radio">
-                                        <label for="hidden">
-                                            <input id="hidden" name="privacy" type="radio"> Ocultar el importe del bote
-                                        </label>
+                                    <div class="form-group">
+                                        <label>Opciones de privacidad</label>
+                                        <div class="radio">
+                                            <label for="hidden">
+                                                <input id="hidden" name="privacy" type="radio"> Ocultar la identidad de los participantes
+                                            </label>
+                                        </div>
+                                        <div class="radio">
+                                            <label for="hidden">
+                                                <input id="hidden" name="privacy" type="radio"> Ocultar el importe con el que colaboran los participantes
+                                            </label>
+                                        </div>
+                                        <div class="radio">
+                                            <label for="hidden">
+                                                <input id="hidden" name="privacy" type="radio"> Ocultar el importe del bote
+                                            </label>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
                         <div class="form-group clearfix">
                             <div class="pull-right">
-                                <a href="" class="btn-link">< Volver </a>
+                                <a href="{{route('moneybox.create')}}" class="btn-link">< Volver </a>
                                 <button class="btn btn-primary">Siguiente ></button>
                             </div>
                         </div>
