63989db5ddf3e90fe69cc919689502c9926954f2
Daniel <daniel.perez.atanacio@gmail.com>
2016-08-16 21:52:42 -0500

    dev: Add favicons

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -92,19 +92,17 @@
         <script src="{{asset('/build/js/scripts.min.js')}}"></script>
 
         <!-- AngularJS -->
-            <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
-            <!-- Facebook SDK -->
-            <script src="{{asset('/js/facebook.js')}}"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
+        <!-- Facebook SDK -->
+        <script src="{{asset('/js/facebook.js')}}"></script>
 
-            <script src="{{asset('/js/coperacha.js')}}"></script>
-            <script src="{{asset('/js/Utils.js')}}"></script>
-            <script src="{{asset('/js/controllers/ModalController.js')}}"></script>
-            <script src="{{asset('/js/controllers/RegisterController.js')}}"></script>
-            <script src="{{asset('/js/controllers/LoginController.js')}}"></script>
-            <script src="{{asset('/js/services/RegisterService.js')}}"></script>
-            <script src="{{asset('/js/services/LoginService.js')}}"></script>
-            <script src="{{asset('/js/directives/CoperachaModal.js')}}"></script>
-
-
+        <script src="{{asset('/js/coperacha.js')}}"></script>
+        <script src="{{asset('/js/Utils.js')}}"></script>
+        <script src="{{asset('/js/controllers/ModalController.js')}}"></script>
+        <script src="{{asset('/js/controllers/RegisterController.js')}}"></script>
+        <script src="{{asset('/js/controllers/LoginController.js')}}"></script>
+        <script src="{{asset('/js/services/RegisterService.js')}}"></script>
+        <script src="{{asset('/js/services/LoginService.js')}}"></script>
+        <script src="{{asset('/js/directives/CoperachaModal.js')}}"></script>
     </body>
 </html>
