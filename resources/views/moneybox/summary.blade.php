64764ad35f7e4341405383ee2d67fb3dd8c84c73
Daniel <daniel.perez.atanacio@gmail.com>
2016-08-17 22:49:37 -0500

    dev: Add summary view - step 2

--- resources/views/moneybox/summary.blade.php
+++ resources/views/moneybox/summary.blade.php
@@ -2,61 +2,88 @@
 @section("content")
     @include('partials.header')
     
-    <section class="block request-view">
+    <section class="block summary-view">
         <div class="holder">
             <div class="container-fluid">
                 <div class="header-block">
-                    <div class="title"></div>
+                    <h2 class="title">Verifica por favor que los datos sean correctos</h2>
                 </div>
                 <div class="content-block">
-                    <form action="#" class="form">
-                        <div class="form-group">
-                            <p class="text-info">Verifica por favor que los datos sean correctos</p>
-                        </div>
-
+                    <form action="#" class="form summary">
                         <div class="form-group">
                             <label for="moneybox">Nombre de la alcancía:</label><input id="moneybox" readonly type="text" value="Demo 18373">
                         </div>
                         
                         <div class="form-group">
-                            <label for="name">Tu Nombre:</label><input id="name" readonly type="text" value="Sanchez Gonzalez">
+                            <label for="name">Tu Nombre: </label><input id="name" readonly type="text" value="Sanchez Gonzalez">
                         </div>
                                           
                         <div class="form-group">
-                            <label for="email">Correo:</label><input id="email" readonly type="text" value="something@company.com">
+                            <label for="email">Correo: </label><input id="email" readonly type="text" value="something@company.com">
                         </div>
                          
                         <div class="form-group">
-                            <label for="mobile">Celular:</label><input id="mobile" readonly type="text" value="243877348">
+                            <label for="mobile">Celular: </label><input id="mobile" readonly type="text" value="243877348">
                         </div>
                     
                         <div class="form-group">
-                            <label for="amount">Tu participacón:</label><input id="amount" readonly type="text" value="100">
+                            <label for="amount">Tu participación: </label><input id="amount" readonly type="text" value="100">
                         </div>
                         
                         <div class="form-group">
-                            <a class="btn-link">editar</button>
-                        </div>
-                        
-                         <div class="form-group">
-                            <label>Selecciona tu método de pago:</label>
-                            <div class="radio">
-                                <label for="payment-oxxo"><input id="payment-oxxo" name="payment" type="radio" value="O"> OXXO</label>
-                            </div>
-                            <div class="radio">
-                                <label for="payment-paypal"><input id="payment-paypal" name="payment" type="radio" value="P"> PAYPAL</label>
-                            </div>
-                            <div class="radio">
-                                <label for="payment-spei"><input id="payment-spei" name="payment" type="radio" value="S"> SPEI</label>
-                            </div>
-                        </div>
-                        
-                        <div class="form-group clearfix">
-                            <div class="pull-right">
-                                <button class="btn btn-primary small">Realizar Pago</button>
-                            </div>
+                            <a class="btn-link btn-edit">
+                                <span class="icon-edit"></span>
+                                <div class="text">Modificar datos</div>
+                            </a>
                         </div>
                     </form>
+
+                    <form action="#" class="form">
+                        <div class="form-group">
+                            <label>Selecciona tu método de pago:</label>
+                            <div class="radio">
+                                <label for="payment-oxxo"><input id="payment-oxxo" name="payment" type="radio" value="O">
+                                    <img src="/images/icon-oxxo.png">
+                                </label>
+                            </div>
+                            <div class="radio">
+                                <label for="payment-paypal"><input id="payment-paypal" name="payment" type="radio" value="P">
+                                    <img src="/images/icon-paypal.png">
+                                </label>
+                            </div>
+                            <div class="radio">
+                                <label for="payment-spei"><input id="payment-spei" name="payment" type="radio" value="S">
+                                    <img src="/images/icon-spei.png">
+                                </label>
+                            </div>
+                        </div>
+
+                        <div class="form-group clearfix">
+                            <div class="clearfix">
+                                <button class="pull-right btn btn-primary small">Realizar Pago</button>
+                            </div>
+                            <div class="small-text">*Al proceder al pago aceptas nuestros términos y condiciones de uso</div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="block payment-methods inverse">
+        <div class="holder">
+            <div class="container-fluid">
+                <div class="header-block">
+                    <div class="title">Formas de Pago</div>
+                </div>
+                <div class="content-block">
+                    <div class="row">
+                        <div class="method"><img src="/images/master-card-logo.png" alt="Master Card"></div>
+                        <div class="method"><img src="/images/american-express-logo.png" alt="American Express"></div>
+                        <div class="method"><img src="/images/visa-logo.png" alt="Visa"></div>
+                        <div class="method"><img src="/images/paypal-logo.png" alt="PayPal"></div>
+                        <div class="method"><img src="/images/oxxo-logo.png" alt="Oxxo"></div>
+                    </div>
                 </div>
             </div>
         </div>
