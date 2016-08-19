759f73b43350676748545d233debdfc01697db37
Daniel <daniel.perez.atanacio@gmail.com>
2016-08-18 23:48:44 -0500

    fix: Add social links in templates

--- resources/views/emails/header.blade.php
+++ resources/views/emails/header.blade.php
@@ -3,9 +3,15 @@
         <a href="{{route('pages.index')}}" style='color:#FF5000;text-decoration:none'>Ver en linea</a>
     </td>
     <td style="padding: 5px; text-align: right;">
-        <a href='#'><img width="30" height="30" src="{{asset('/images/emails/fb.png')}}" alt='facebook'/></a>
-        <a href='#'><img width="30" height="30" src="{{asset('/images/emails/in.png')}}" alt='linkedin'/></a>
-        <a href='#'><img width="30" height="30" src="{{asset('/images/emails/twitter.png')}}" alt='twitter'/></a>
+        <a style="display: inline-block" href="https://www.facebook.com/Coperacha-Mx-444730879054310/?fref=ts">
+            <img width="30" height="30" src="{{asset('/images/emails/fb.png')}}" alt='facebook'/>
+        </a>
+        <a style="display: inline-block" href="https://www.instagram.com/coperachamx/">
+            <img width="30" height="30" src="{{asset('/images/emails/in.png')}}" alt='linkedin'/>
+        </a>
+        <a style="display: inline-block" href="https://twitter.com/coperacha_mx">
+            <img width="30" height="30" src="{{asset('/images/emails/twitter.png')}}" alt='twitter'/>
+        </a>
     </td>
 </tr>
 <tr>
