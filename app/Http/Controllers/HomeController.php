3a927d9a6f0a299abc4529b141c3d08af66d55cd
Emmanuel <sanchezz985@gmail.com>
2016-08-30 00:28:11 -0500

    > fix contact emails

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -224,10 +224,10 @@ class HomeController extends Controller
             'content' => $request->get('content'),
         ];
 
-        Mail::send('emails.contact', $data, function ($message) {
-            $message->from('contacto@coperacha.com.mx', 'Coperacha');
-            $message->to('perezatanaciod@gmail.com', 'Daniel');
-            $message->cc('sanchezz985@gmail.com', 'Emmanuel');
+        Mail::send('emails.contact', $data, function ($message) use($request){
+            $message->from($request->get('email'), 'Coperacha');
+            $message->to('coperachamexico@gmail.com');
+            $message->bcc(array('sanchezz985@gmail.com','perezatanaciod@gmail.com'));
             $message->subject('Mensaje de Contacto');
         });
 
