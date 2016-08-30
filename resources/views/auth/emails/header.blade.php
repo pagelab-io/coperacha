59839508fc634e7e91fd64ab453833a008f27208
Emmanuel <sanchezz985@gmail.com>
2016-08-29 23:59:18 -0500

    > add views for password recovery

---
+++ resources/views/auth/emails/header.blade.php
@@ -0,0 +1,21 @@
+<tr>
+    <td>
+        <a href="{{route('pages.index')}}" style='color:#FF5000;text-decoration:none'>Ver en linea</a>
+    </td>
+    <td style="padding: 5px; text-align: right;">
+        <a style="display: inline-block" href="https://www.facebook.com/coperachamx">
+            <img width="30" height="30" src="{{asset('/images/emails/fb.png')}}" alt='facebook'/>
+        </a>
+        <a style="display: inline-block" href="https://www.instagram.com/coperachamx/">
+            <img width="30" height="30" src="{{asset('/images/emails/in.png')}}" alt='linkedin'/>
+        </a>
+        <a style="display: inline-block" href="https://twitter.com/coperacha_mx">
+            <img width="30" height="30" src="{{asset('/images/emails/twitter.png')}}" alt='twitter'/>
+        </a>
+    </td>
+</tr>
+<tr>
+    <td COLSPAN=2 style="padding-top: 30px; padding-bottom: 10px; text-align: center">
+        <a href='#'><img width="147" height="90" src={{asset('/images/emails/logo.png')}} alt='logo'/></a>
+    </td>
+</tr>
