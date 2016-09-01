e7e539148629e86a2efbc70534a2de009097319c
Daniel <daniel.perez.atanacio@gmail.com>
2016-08-31 23:49:37 -0500

    dev:  Invitation - step 4

--- resources/views/emails/invitation.blade.php
+++ resources/views/emails/invitation.blade.php
@@ -2,17 +2,20 @@
 @section('title', 'Invitación a alcancía')
 @section('body')
     <tr>
+        <td COLSPAN=2 align="center" height="1" style="font-size:1px; line-height:1px; background: #CCCCCC"></td>
+    </tr>
+    <tr>
         <td COLSPAN=2 style="font-family: Arial;font-size: 30px;color: #FF5000;text-align: center;line-height: 30px;padding-top:45px;padding-bottom:15px;">
             <a href="{{asset('/moneybox/detail/'.$moneybox->url)}}" style='color:#FF5000;text-decoration:none;'>{{$moneybox->name}}</a>
         </td>
     </tr>
     <tr>
-        <td COLSPAN=2 align="center" height="1" style="font-size:1px; line-height:1px; background: #CCCCCC"></td>
-    </tr>
-    <tr>
         <td COLSPAN=2 style="font-family: Arial;font-size: 16px;color: #666666;text-align: justify;line-height: 28px; padding-top:10px;">
             <p>Coperacha es una plataforma en línea que facilita la recaudación de dinero entre un grupo de personas.</p>
-            <p>{{$moneybox->person->name}} te invitó a que te unas a la alcancía <strong>{{$moneybox->name}} </strong> la cual busca {{$moneybox->description}}</p>
+            <p>
+                {{$moneybox->person->name}} te invitó a que te unas a la alcancía <strong>{{$moneybox->name}} </strong>
+                la cual busca <strong>{{$moneybox->description}}<strong>
+            </p>
             <p>
                 <span>Encuentra todos los detalles en:</span>
                 <strong>
--- resources/views/emails/pendinginvitation.blade.php
+++ resources/views/emails/pendinginvitation.blade.php
@@ -10,7 +10,8 @@
             <p>
                 Hola, observamos que tienes una invitación pendiente para
                 participar en la alcancía llamada <strong>{{$moneybox->name}}</strong>
-                creada por <strong>{{$moneybox->person->name}}</strong>, la cual busca {{$moneybox->description}}.
+                creada por <strong>{{$moneybox->person->name}}</strong>, la cual busca
+                <strong>{{$moneybox->description}}</strong>.
             </p>
             <p>
                 Para poder participar entra al siguiente link:<strong><a href="{{asset('/moneybox/detail/'.$moneybox->url)}}" style='color:#FF5000;text-decoration:none;'>{{$moneybox->name}}</a></strong>
