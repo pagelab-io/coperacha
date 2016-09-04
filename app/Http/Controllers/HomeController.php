67389b20cb46bf2845cf6148c76e3d260c8f9b95
Daniel <daniel.perez.atanacio@gmail.com>
2016-09-03 21:40:27 -0500

    dev:  Request money feature - step 3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -291,15 +291,13 @@ class HomeController extends Controller
         ];
 
         if (true == $withMail) {
-
             Mail::send('emails.request', $data, function ($message) use ($user, $file) {
-
                 $pdf = Storage::disk('public')->get($file->name);
                 $message->from($user->email, $user->username);
                 //$message->to('coperachamexico@gmail.com');
                 $message->to(['perezatanaciod@gmail.com']); // 'sanchezz985@gmail.com',
                 $message->subject('Solicitud de Retiro');
-                $message->attach($pdf, ['display' => $pdf->name, 'mime' => $pdf->metadata]);
+                $message->attach($pdf, ['display' => $file->name, 'mime' => $file->metadata]);
             });
         }
 
