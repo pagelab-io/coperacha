669a7eee16e2bcc7a5cf5daaeb83d48c3867f29c
Emmanuel <sanchezz985@gmail.com>
2016-09-13 22:50:22 -0500

    change dashboard styles

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
     <link href="{{asset('/css/bootstrap.css')}}" rel="stylesheet">
     <!-- Styles -->
-    <link href="/css/dashboard.css" rel="stylesheet">
+    <link href="{{asset('css/dashboard.css')}}" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
