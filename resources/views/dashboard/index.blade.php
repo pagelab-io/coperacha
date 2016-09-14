c510aacfd87ba113769777737a40800adfebb850
Emmanuel <sanchezz985@gmail.com>
2016-09-13 22:32:28 -0500

    change in dashboard index

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,122 +6,166 @@
             <h3 class="title-1">Dashboard</h3>
             <hr>
             <div class="items">
-                <div class="item">
-                    <header>
-                        <div class="item-title">Usuarios del servicio</div>
-                        <div class="item-desc">Número total de usuarios (inscritos/activos) por genero, edad, ciudad</div>
-                    </header>
-                    <main>
-                        <table class="table table-striped table-responsive">
-                            <caption>
-                                <span class="small">Total de Usuarios: {{$users['total']}}</span>
-                            </caption>
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Genero</th>
-                                    <th>Cantidad</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($users['data'] as $i => $row)
+
+                <div class="dashboard row">
+                    <div class="col-xs-12 col-sm-6">
+                        <div class="panel panel-primary">
+
+                            <!-- header -->
+                            <div class="panel-heading">
+                                Usuarios del servicio
+                                <br>
+                                <span>Número total de usuarios (inscritos/activos) por genero, edad, ciudad</span>
+                            </div>
+                            <!-- body -->
+                            <div class="panel-body">
+                                <!-- Table -->
+                                <table class="table table-striped table-responsive">
+                                    <caption>
+                                        <span class="small">Total de Usuarios: {{$users['total']}}</span>
+                                    </caption>
+                                    <thead>
+                                        <tr>
+                                            <th class="widget-th">#</th>
+                                            <th class="widget-th">Genero</th>
+                                            <th class="widget-th">Cantidad</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($users['data'] as $i => $row)
+                                            <tr>
+                                                <td>{{++$i}}</td>
+                                                <td>{{$row->gender}}</td>
+                                                <td>{{$row->qty}}</td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!--footer-->
+                            <div class="panel-footer">
+                                Ver más <span class="glyphicon glyphicon-arrow-right"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                        <div class="panel panel-primary">
+
+                            <!-- header -->
+                            <div class="panel-heading">
+                                alcancías
+                                <br>
+                                <span>Cuantas alcancías se crean diario, mensual, anual</span>
+                            </div>
+                            <!-- body -->
+                            <div class="panel-body">
+                                <!-- Table -->
+                                <table class="table table-striped table-responsive">
+                                    <caption>
+                                        <div>Creación de alcancías promedio</div>
+                                        <span class="small"></span>
+                                    </caption>
+                                    <thead>
                                     <tr>
-                                        <td>{{++$i}}</td>
-                                        <td>{{$row->gender}}</td>
-                                        <td>{{$row->qty}}</td>
+                                        <th class="widget-th">#</th>
+                                        <th class="widget-th">Alcancías</th>
+                                        <th class="widget-th">Cantidad</th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </main>
-                </div>
-                <div class="item">
-                    <header>
-                        <div class="item-title">alcancías</div>
-                        <div class="item-desc">Cuantas alcancías se crean diario, mensual, anual</div>
-                    </header>
-                    <main>
-                        <table class="table table-striped table-responsive">
-                            <caption>
-                                <div>Creación de alcancías promedio</div>
-                                <span class="small"></span>
-                            </caption>
-                            <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Alcancías</th>
-                                <th>Cantidad</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($moneyboxes as $type => $value)
-                                <tr>
-                                    <td></td>
-                                    <td>{{$type}}</td>
-                                    <td>{{$value}}</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </main>
-                </div>
-                <div class="item">
-                    <header>
-                        <div class="item-title">Durabiliad</div>
-                        <div class="item-desc">La durabilidad promedio, el monto promedio recaudado (diario, mensual, anual), la coperacha promedio por alcancía</div>
-                    </header>
-                    <main>
-                        <table class="table table-striped table-responsive">
-                            <caption>
-                            </caption>
-                            <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Tipo</th>
-                                <th>Promedio</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($durability as $type => $value)
-                                <tr>
-                                    <td></td>
-                                    <td>{{$type}}</td>
-                                    <td>{{$value}}</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </main>
-                </div>
-                <div class="item">
-                    <header>
-                        <div class="item-title">Métodos de Pago</div>
-                        <div class="item-desc">Porcentaje de uso</div>
-                    </header>
-                    <main>
-                        <table class="table table-striped table-responsive">
-                            <caption>
-                            </caption>
-                            <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Método de Pago</th>
-                                <th>Valor Promedio</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($payments as $index => $row)
-                                <tr>
-                                    <td></td>
-                                    <td>{{$row->method}}</td>
-                                    <td>{{number_format($row->percent, 2)}} %</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </main>
-                </div>
-            </div>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($moneyboxes as $type => $value)
+                                        <tr>
+                                            <td></td>
+                                            <td>{{$type}}</td>
+                                            <td>{{$value}}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!--footer-->
+                            <div class="panel-footer">Panel footer</div>
+                        </div>
+                    </div>
+                </div> <!-- end row-->
+
+                <div class="dashboard row">
+                    <div class="col-xs-12 col-sm-6">
+                        <div class="panel panel-primary">
+
+                            <!-- header -->
+                            <div class="panel-heading">
+                                Durabilidad
+                                <br>
+                                <span>La durabilidad promedio, el monto promedio recaudado (diario, mensual, anual), la coperacha promedio por alcancía</span>
+                            </div>
+                            <!-- body -->
+                            <div class="panel-body">
+                                <!-- Table -->
+                                <table class="table table-striped table-responsive">
+                                    <caption>
+                                    </caption>
+                                    <thead>
+                                    <tr>
+                                        <th class="widget-th">#</th>
+                                        <th class="widget-th">Tipo</th>
+                                        <th class="widget-th">Promedio</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($durability as $type => $value)
+                                        <tr>
+                                            <td></td>
+                                            <td>{{$type}}</td>
+                                            <td>{{$value}}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!--footer-->
+                            <div class="panel-footer">Panel footer</div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                        <div class="panel panel-primary">
+
+                            <!-- header -->
+                            <div class="panel-heading">
+                                métodos de pago
+                                <br>
+                                <span>Porcentaje de uso</span>
+                            </div>
+                            <!-- body -->
+                            <div class="panel-body">
+                                <!-- Table -->
+                                <table class="table table-striped table-responsive">
+                                    <caption>
+                                    </caption>
+                                    <thead>
+                                    <tr>
+                                        <th class="widget-th">#</th>
+                                        <th class="widget-th">Método de Pago</th>
+                                        <th class="widget-th">Valor Promedio</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($payments as $index => $row)
+                                        <tr>
+                                            <td></td>
+                                            <td>{{$row->method}}</td>
+                                            <td>{{number_format($row->percent, 2)}} %</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!--footer-->
+                            <div class="panel-footer">Panel footer</div>
+                        </div>
+                    </div>
+                </div> <!-- end row-->
+            </div><!--end items-->
         </section>
     </div>
 @stop
