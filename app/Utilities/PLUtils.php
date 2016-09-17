be62cb6887604b261b9f19d6c88123e2a9b91007
Emmanuel <sanchezz985@gmail.com>
2016-09-17 14:41:36 -0500

    > add method for get the user's age > add panel for moneybox participations

--- app/Utilities/PLUtils.php
+++ app/Utilities/PLUtils.php
@@ -2,6 +2,8 @@
 
 namespace App\Utilities;
 
+use Carbon\Carbon;
+
 class PLUtils {
 
     /**
@@ -18,4 +20,60 @@ class PLUtils {
         }
     }
 
-} 
+    /**
+     * @param $date - 2016-09-12 21:48:02
+     * @param string $format
+     * @return string
+     */
+    public static function getStringDate($date, $format = '')
+    {
+        $date = explode('-', $date);
+        switch($format){
+            case 'mm-YY' : return self::getMonthToString($date[1])." ".$date[0];
+            default: return explode(' ',$date[2])[0]." de ".self::getMonthToString($date[1])." de ".$date[0];
+        }
+
+    }
+
+    /**
+     * Gets a date in YYYY-mm-dd format and return the age
+     * @param $date
+     * @return int|string
+     */
+    public static function getAge($date)
+    {
+        if ($date == '0000-00-00')
+            return '--';
+
+        $dates = explode('-', $date);
+        return Carbon::createFromDate($dates[0],$dates[1],$dates[2])->age." años";
+    }
+
+    //region private methods
+
+    /**
+     * Gets a number and return the string name for month
+     * @param $month - 01
+     * @return string - Enero
+     */
+    private static function getMonthToString($month)
+    {
+        switch($month){
+            case '01' : return 'Enero';  break;
+            case '02' : return 'Febrero';  break;
+            case '03' : return 'Marzo';  break;
+            case '04' : return 'Abril';  break;
+            case '05' : return 'Mayo';  break;
+            case '06' : return 'Junio';  break;
+            case '07' : return 'Julio';  break;
+            case '08' : return 'Agosto';  break;
+            case '09' : return 'Septiembre';  break;
+            case '10' : return 'Octubre';  break;
+            case '11' : return 'Noviembre';  break;
+            case '12' : return 'Diciembre';  break;
+            default   : return 'Enero';  break;
+        }
+    }
+    //endregion
+
+}
--- resources/views/dashboard/user/detail.blade.php
+++ resources/views/dashboard/user/detail.blade.php
@@ -4,7 +4,7 @@
     <!-- Title and description-->
     <div class="dashboard-titles">
         <h2>{{$user->person->name}} {{$user->person->lastname}}</h2>
-        <p>Miembro desde 17/09/2016</p>
+        <p>Miembro desde {{\App\Utilities\PLUtils::getStringDate($user->person->created_at, 'mm-YY')}}</p>
     </div>
 
     <section class="dashboard row">
@@ -17,6 +17,8 @@
                 <!-- header -->
                 <div class="panel-heading">
                     Información general
+                    <br>
+                    <span>Información proporcionada por el usuario desde el momento de su registro.</span>
                 </div>
 
                 <!-- body -->
@@ -38,7 +40,7 @@
                             </tr>
                             <tr>
                                 <td>Edad</td>
-                                <td>{{$user->person->birthday}}</td>
+                                <td>{{\App\Utilities\PLUtils::getAge($user->person->birthday)}}</td>
                             </tr>
                             <tr>
                                 <td>Ciudad</td>
@@ -63,6 +65,8 @@
                 <!-- header -->
                 <div class="panel-heading">
                     Alcancías creadas
+                    <br>
+                    <span>Información de las alcancías creadas por el usuario.</span>
                 </div>
 
                 <!-- body -->
@@ -77,6 +81,42 @@
                         </thead>
                         <tbody>
                             @foreach($my_moneyboxes as $moneybox)
+                            <tr>
+                                <td>{{$moneybox->id}}</td>
+                                <td>{{$moneybox->name}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+
+        <!--more relevant info-->
+        <div class="dashboard more-info">
+
+            <div class="panel panel-primary">
+
+                <!-- header -->
+                <div class="panel-heading">
+                    Alcancías en las que participa
+                    <br>
+                    <span>Información de las alcancías en las que el usuario a participado.</span>
+                </div>
+
+                <!-- body -->
+                <div class="panel-body">
+                    <!-- Table -->
+                    <table class="table table-striped table-responsive">
+                        <thead>
+                            <tr>
+                                <th class="widget-th">#</th>
+                                <th class="widget-th">Nombre alcancía</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($moneyboxes_participation as $moneybox)
                             <tr>
                                 <td>{{$moneybox->id}}</td>
                                 <td>{{$moneybox->name}}</td>
