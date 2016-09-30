af4eadee7feba4710f4492f56a087d692b7e217e
pagelab.io <pagelab-io@gmail.com>
2016-09-29 21:13:09 -0500

    dev:  Add cms test - config

---
+++ users.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM coperacha.wp_users
+LIMIT 0, 1000
+
+-- Date: 2016-09-29 21:12
+*/
+INSERT INTO `wp_users` (`ID`,`user_login`,`user_pass`,`user_nicename`,`user_email`,`user_url`,`user_registered`,`user_activation_key`,`user_status`,`display_name`) VALUES (1,'developer','$P$BA.DB7Wo.h3VGIy4kTx6IaowMIghNU/','developer','perezatanaciod@gmail.com','','2016-09-29 11:23:58','',0,'developer');
