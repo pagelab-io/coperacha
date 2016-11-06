3e8af88dc85cb8ed9a226a715c2a2fc2ce146b35
Emmanuel <sanchezz985@gmail.com>
2016-11-06 17:32:49 -0600

    > add pure chat

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -154,6 +154,7 @@
 </div>
 <!-- Small Alert Dialog -->
 <!-- Include jQuery-->
+<script type='text/javascript' data-cfasync='false'>window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: 'c75eee33-a375-49dc-a2c7-7ab606122aa2', f: true }); done = true; } }; })();</script>
 <script src="{{asset('/js/vendor/jquery/jquery-3.1.0.js')}}"></script>
 <script src="{{asset('/js/vendor/jquery/jquery-ui.js')}}"></script>
 
