b0cae1e96314fefbf41b45f69cf08500add47ea6
Emmanuel <sanchezz985@gmail.com>
2016-11-09 21:18:18 -0600

    > fix bug

--- resources/views/emails/header.blade.php
+++ resources/views/emails/header.blade.php
@@ -1,5 +1,5 @@
 <tr style="background: #FF5000">
     <td style="padding-top: 30px; padding-bottom: 10px; text-align: center">
-        <a href='"{{route('pages.index')}}"'><img src={{asset('/images/logo.png')}}/></a>
+        <a href='"{{route('pages.index')}}"'><img src="{{asset('/images/logo.png')}}"/></a>
     </td>
 </tr>
