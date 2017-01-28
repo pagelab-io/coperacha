e361f9c04f4f054413830df8c91292220752b70a
Emmanuel <sanchezz985@gmail.com>
2017-01-27 23:28:54 -0600

    new daily logging and new logging class

---
+++ app/Utilities/PLCustomLog.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by Emmanuel Sánchez Luna.
+ * Github user: sanchezz985
+ * Date: 27/01/2017
+ * Time: 23:15
+ */
+
+namespace App\Utilities;
+
+use Illuminate\Support\Facades\Log;
+
+/**
+ * Class for logging with the className or file where log is used.
+ * Class PLCustomLog
+ * @package App\Utilities
+ */
+class PLCustomLog
+{
+
+    /**
+     * @var String
+     */
+    private $_className;
+
+    /**
+     * PLCustomLog constructor.
+     * @param $classname
+     */
+    public function __construct($classname)
+    {
+        $this->_className = $classname;
+    }
+
+    /**
+     * Print message in laravel.log
+     * @param $message
+     */
+    public function info($message)
+    {
+        Log::info("[".$this->_className."] ".$message);
+    }
+
+}
